6ce8d993666c7ab166e02a9f27b71e852515c747
Lheo <leo9266@gmail.com>
2021-06-25 21:24:16 -0300

    Commit com o App e package atualizados

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import CorpoInstr from './modulos/components/CorpoInstr';
+import Tempo from "./modulos/modelos/Tempo.js"
+import Som from './modulos/modelos/Som.js'
+import { Sound } from 'expo-av/build/Audio';
+
 
 export default function App() {
+  const som = new Som()
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CorpoInstr
+        tempo={new Tempo(som.tocarSom, som.pararSom)}
+        tocarSom = {som.tocarSom}
+        sons={Object.keys(som.sons)}
+      />
     </View>
   );
 }
